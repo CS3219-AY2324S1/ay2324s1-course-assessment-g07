7e392138d19c8c547f89ad4b826c02f2c630e9a6
CheongYeeMing <e0526001@u.nus.edu>
2023-09-28 16:48:36 +0800

    Add Landing Page

---
+++ frontend/app/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Image from 'next/image';
+import LandingPageImage from '../../images/LandingPage.png';
+import GetStartedButton from './GetStartedButton';
+import LandingPageNavBar from '../NavigationBar/NavigationBar';
+
+const LandingPage = () => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex md:flex-row flex-col">
+        <LandingPageNavBar />
+      </div>
+      <div className="container mx-auto flex md:flex-row flex-col items-center pt-36">
+        <div className="ml-60 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-5xl text-5xl mb-4 font-bold text-gray-900">
+            Coding Interviews
+            <br className="hidden lg:inline-block" />
+            Made Fun
+          </h1>
+          <p className="mb-8 leading-relaxed text-xl">
+            Excel in your technical interviews today!
+          </p>
+          <div className="flex justify-center">
+            <GetStartedButton />
+          </div>
+        </div>
+        <div className="mr-60 lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <Image
+            src={LandingPageImage}
+            alt="hero"
+            className="object-cover object-center rounded"
+            width={720}
+            height={600}
+          ></Image>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LandingPage;
