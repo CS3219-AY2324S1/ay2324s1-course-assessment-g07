b9e4f0f7406c18db6170a41a9131ce108f7b11c1
imraj singh <92265647+imrajsinghsandhu@users.noreply.github.com>
2023-10-02 16:17:03 +0800

    initialise firebase app and connect to it

--- chat-service/index.js
+++ chat-service/index.js
@@ -4,3 +4,17 @@
  * 2. User2 id
  * 3. sessionId
  */
+const { initializeApp } = require("firebase/app");
+require('dotenv').config();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
