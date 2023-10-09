322c4b7cf4bca87b87a4b6cc6f80fbcdbe5927b0
CheongYeeMing <e0526001@u.nus.edu>
2023-10-09 18:04:30 +0800

    Edit Modal Button CSS

--- frontend/app/components/Matchmaking/WaitingModal.tsx
+++ frontend/app/components/Matchmaking/WaitingModal.tsx
@@ -1,90 +1,118 @@
-'use client'
-import React from "react";
-import { useState, useEffect } from "react";
+'use client';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
+const WaitingModal = ({
+  averageWaitingTime,
+  modalStatus,
+  handleClose,
+  handleRetry,
+  handleCancelSearch,
+}: any) => {
+  const waitingTimeLimit = 10;
+  const redirectTimeLimit = 5;
+  const [searchTimer, setSearchTimer] = useState(waitingTimeLimit); // Initial timer value in seconds
+  const [redirectTimer, setRedirectTimer] = useState(redirectTimeLimit); // Initial timer value in seconds
 
-const WaitingModal = ( { averageWaitingTime, modalStatus, handleClose, handleRetry, handleCancelSearch } :  any) => {
-    const waitingTimeLimit = 10;
-    const redirectTimeLimit = 5;
-    const [searchTimer, setSearchTimer] = useState(waitingTimeLimit); // Initial timer value in seconds
-    const [redirectTimer, setRedirectTimer] = useState(redirectTimeLimit); // Initial timer value in seconds
+  useEffect(() => {
+    if (modalStatus == 'searching') {
+      const interval = setInterval(() => {
+        setSearchTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 0.01 : 0));
+      }, 10);
 
-    useEffect(() => {
-        if (modalStatus == 'searching') {
-            const interval = setInterval(() => {
-                setSearchTimer(prevTimer => prevTimer > 0 ? prevTimer - 0.01 : 0); 
-            }, 10);
+      return () => {
+        clearInterval(interval);
+      };
+    } else if (modalStatus == 'success') {
+      const interval = setInterval(() => {
+        setRedirectTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 0.01 : 0));
+      }, 10);
 
-            return () => {
-                clearInterval(interval);
-            };
-        } else if (modalStatus == 'success') {
-            const interval = setInterval(() => {
-                setRedirectTimer(prevTimer => prevTimer > 0 ? prevTimer - 0.01 : 0); 
-            }, 10);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [modalStatus]);
 
-            return () => {
-                clearInterval(interval);
-            };
-        }      
-    }, [modalStatus]);
+  useEffect(() => {
+    if (modalStatus != 'searching') setSearchTimer(waitingTimeLimit);
 
-    useEffect(() => {
-        if (modalStatus != 'searching') setSearchTimer(waitingTimeLimit);  
+    if (modalStatus == 'success') setRedirectTimer(redirectTimeLimit);
+  }, [modalStatus]);
 
-        if (modalStatus == 'success') setRedirectTimer(redirectTimeLimit);
-    }, [modalStatus]);
-    
-    return (      
+  return (
     <dialog id="my_modal_1" className="modal">
-
-        <div className="modal-box text-center flex flex-col items-center justify-center">
-            { modalStatus != "success" &&
-            <div className="radial-progress m-4 text-success" style={{"--value":searchTimer * 100 / waitingTimeLimit}}>{searchTimer.toFixed(0)}</div>
+      <div className="modal-box text-center flex flex-col items-center justify-center">
+        {modalStatus != 'success' && (
+          <div
+            className="radial-progress m-4 text-success"
+            style={{ '--value': (searchTimer * 100) / waitingTimeLimit } as any}
+          >
+            {searchTimer.toFixed(0)}
+          </div>
+        )}
+        {modalStatus == 'success' && (
+          <div
+            className="radial-progress m-4 text-success"
+            style={
+              { '--value': (redirectTimer * 100) / redirectTimeLimit } as any
             }
-            { modalStatus == "success" &&
-            <div className="radial-progress m-4 text-success" style={{"--value":redirectTimer * 100 / redirectTimeLimit}}>{redirectTimer.toFixed(0)}</div>
-            }
+          >
+            {redirectTimer.toFixed(0)}
+          </div>
+        )}
 
-            { modalStatus == "searching" &&
-            <h3 className="font-bold text-lg text-center">
-                Finding an opponent...
-            </h3>}
-            { modalStatus == "success" &&
-            <h3 className="font-bold text-lg text-center">
-                Found an opponent! Joining the session in...
-            </h3>}
-            { modalStatus == "canceled" &&
-            <h3 className="font-bold text-lg text-center">
-                Failed to find an opponent! 
-            </h3>}
-            <p className="py-4 text-xs text-center">
-                Average waiting time: {averageWaitingTime == null ? 0 : averageWaitingTime > 10 ? "10+" : averageWaitingTime.toFixed(1)}s
-            </p>
-            {modalStatus == "canceled" && <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {modalStatus == 'searching' && (
+          <h3 className="font-bold text-lg text-center">
+            Finding an opponent...
+          </h3>
+        )}
+        {modalStatus == 'success' && (
+          <h3 className="font-bold text-lg text-center">
+            Found an opponent! Joining the session in...
+          </h3>
+        )}
+        {modalStatus == 'canceled' && (
+          <h3 className="font-bold text-lg text-center">
+            Failed to find an opponent!
+          </h3>
+        )}
+        <p className="py-4 text-xs text-center">
+          Average waiting time:{' '}
+          {averageWaitingTime == null
+            ? 0
+            : averageWaitingTime > 10
+            ? '10+'
+            : averageWaitingTime.toFixed(1)}
+          s
+        </p>
+        {modalStatus == 'canceled' && (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
             <button
-                className={`btn btn-outline btn-info btn-block mb-2 btn-active m-2`}
-                onClick={() => handleRetry()}
+              className={`btn btn-outline btn-primary btn-block mb-2 m-2`}
+              onClick={() => handleRetry()}
             >
-            Retry
+              Retry
             </button>
             <button
-                className={`btn btn-outline btn-error btn-block mb-2 btn-active m-2`}
-                onClick={() => handleClose()}
+              className={`btn btn-outline btn-error btn-block mb-2 m-2`}
+              onClick={() => handleClose()}
             >
-            Close
+              Close
             </button>
-            </div>}
-            {modalStatus == "searching" &&
-            <button
-                className={`btn btn-outline btn-error btn-block mb-2 btn-active`}
-                onClick={() => handleCancelSearch()}
-            >
+          </div>
+        )}
+        {modalStatus == 'searching' && (
+          <button
+            className={`btn btn-outline btn-error btn-block mb-2 `}
+            onClick={() => handleCancelSearch()}
+          >
             Cancel
-            </button>}
-        </div>
-    </dialog>);
-
-}
+          </button>
+        )}
+      </div>
+    </dialog>
+  );
+};
 
 export default WaitingModal;
