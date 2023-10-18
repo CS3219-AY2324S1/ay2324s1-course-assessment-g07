0acac5361201ef0b124fc35dbd9c17701342c7ed
imraj singh <92265647+imrajsinghsandhu@users.noreply.github.com>
2023-10-18 15:20:00 +0800

    Remove unnecessary npm modules in chat service backend
    Dont need exprees, axios, cors etc

--- chat-service/index.js
+++ chat-service/index.js
@@ -1,36 +1,7 @@
-/**
- * Connect to firebase here & get the following from collaboration service:
- * 1. User1 id
- * 2. User2 id
- * 3. sessionId
- */
 const { initializeApp } = require("firebase/app");
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const http = require('http');
 
-app.use(cors());
-app.use(express.json());
-
-// create an endpoint on this localhost:8003 to receive the session information from collaboration service
-app.post('/chat-service-id-information', (req, res) => {
-
-  // const { parsedMessage } = req.body;
-  // // console.log('parsedMessage: ', parsedMessage.data);
-  // // console.log('received session information from collaboration service');
-
-  const { sessionId, user1Info, user2Info } = req.body;
-  console.log('received session information from collaboration service');
-  console.log('sessionId: ', sessionId);
-  console.log('user1Info: ', user1Info);
-  console.log('user2Info: ', user2Info);
-  res.send('received session information from collaboration service');
-});
-
-
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration stored in .env file locally
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
