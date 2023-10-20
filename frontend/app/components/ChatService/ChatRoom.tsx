bc10b61e41430989156c2f985fb9e1ae8bf87c7b
imraj singh <92265647+imrajsinghsandhu@users.noreply.github.com>
2023-10-20 23:26:37 +0800

    enable scroll to bottom in chat upon every new message

--- frontend/app/components/ChatService/ChatRoom.tsx
+++ frontend/app/components/ChatService/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { getFirestore, collection, query, orderBy, addDoc, serverTimestamp } from 'firebase/firestore';
 import ChatMessage from './ChatMessage';
@@ -11,9 +11,10 @@ interface ChatRoomProps {
 const ChatRoom: React.FC<ChatRoomProps> = ({ docRef, userId }) => {
 
   const db = getFirestore();
-  const dummy = useRef<HTMLSpanElement | null>(null);
+  const messageSpanRef = useRef<HTMLElement | null>(null);
   const messagesRef = collection(db, 'matched-tokens', docRef.id, 'messages');
   const [formValue, setFormValue] = useState('');
+
 
   const messagesQuery = query(
     collection(db, 'matched-tokens', docRef?.id, 'messages'),
@@ -21,6 +22,11 @@ const ChatRoom: React.FC<ChatRoomProps> = ({ docRef, userId }) => {
   );
 
   const [messages] = useCollectionData(messagesQuery);
+
+  useEffect(() => {
+    // Scroll to the latest message when messages change
+    messageSpanRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
 
   const sendMessage = async (e: any) => {
     e.preventDefault();
@@ -33,31 +39,32 @@ const ChatRoom: React.FC<ChatRoomProps> = ({ docRef, userId }) => {
         userId
       })
       console.log("Document has been written into Database");
-      dummy.current?.scrollIntoView({ behavior: 'smooth' });
     } catch (error) {
       console.error(error);
     }
   }
 
   return (
-    <div className=''>
+    <div className="">
       <main>
         {messages && messages.map((message, index) => <ChatMessage key={index} message={message} userId={userId} />)}
-        <span ref={dummy} />
+        <span ref={messageSpanRef} />
       </main>
 
       <form className="flex h-10vh bg-gray-900 w-full" onSubmit={sendMessage}>
         <input className="w-full text-white text-base bg-gray-400 outline-none border-none px-4 placeholder-white"
           value={formValue} onChange={(e) => setFormValue(e.target.value)}
-          placeholder="What was your approach in solving this question?"
+          placeholder="What was your approach in solving this question"
         />
         <button
           className="bg-gray-700 border-none text-white p-4 text-2xl cursor-pointer"
           type="submit"
-          disabled={!formValue}>🕊️</button>
+          disabled={!formValue}>🕊️
+        </button>
       </form>
     </div>
-  )
+  );
+
 }
 
 export default ChatRoom;
