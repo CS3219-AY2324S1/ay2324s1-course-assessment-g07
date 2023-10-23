314c48fc4ee19927215b09012619260825afc10b
Lu Yiting <e0774189@u.nus.edu>
2023-10-23 16:22:34 +0800

    Improve matchmaking modal and button

--- frontend/app/components/Matchmaking/WaitingModal.tsx
+++ frontend/app/components/Matchmaking/WaitingModal.tsx
@@ -65,80 +65,6 @@ const WaitingModal = ({
   }, [modalStatus]);
 
   return (
-    // <dialog id="my_modal_1" className="modal">
-    //   <div className="modal-box text-center flex flex-col items-center justify-center">
-    //     {modalStatus != 'success' && (
-    //       <div
-    //         className="radial-progress m-4 text-success"
-    //         style={{ '--value': (searchTimer * 100) / waitingTimeLimit } as any}
-    //       >
-    //         {searchTimer.toFixed(0)}
-    //       </div>
-    //     )}
-    //     {modalStatus == 'success' && (
-    //       <div
-    //         className="radial-progress m-4 text-success"
-    //         style={
-    //           { '--value': (redirectTimer * 100) / redirectTimeLimit } as any
-    //         }
-    //       >
-    //         {redirectTimer.toFixed(0)}
-    //       </div>
-    //     )}
-
-    //     {modalStatus == 'searching' && (
-    //       <h3 className="font-bold text-lg text-center">
-    //         Searching for an opponent...
-    //       </h3>
-    //     )}
-    //     {modalStatus == 'success' && (
-    //       <h3 className="font-bold text-lg text-center">
-    //         Found an opponent! Joining the session in...
-    //       </h3>
-    //     )}
-    //     {modalStatus == 'canceled' && (
-    //       <h3 className="font-bold text-lg text-center">
-    //         Failed to find an opponent!
-    //       </h3>
-    //     )}
-    //     <p className="py-4 text-xs text-center">
-    //       Average waiting time:{' '}
-    //       {averageWaitingTime == null
-    //         ? 0
-    //         : averageWaitingTime > 10
-    //         ? '10+'
-    //         : averageWaitingTime.toFixed(1)}
-    //       s
-    //     </p>
-    //     {modalStatus == 'canceled' && (
-    //       <div style={{ display: 'flex', justifyContent: 'center' }}>
-    //         <button
-    //           className={`btn btn-outline btn-primary btn-block mb-2 m-2`}
-    //           onClick={() => handleRetry()}
-    //         >
-    //           Retry
-    //         </button>
-    //         <button
-    //           className={`btn btn-outline btn-error btn-block mb-2 m-2`}
-    //           onClick={() => handleClose()}
-    //         >
-    //           Close
-    //         </button>
-    //       </div>
-    //     )}
-    //     {modalStatus == 'searching' && (
-    //       <button
-    //         className={`btn btn-outline btn-error btn-block mb-2 `}
-    //         onClick={() => handleCancelSearch()}
-    //       >
-    //         Cancel
-    //       </button>
-    //     )}
-    //   </div>
-    // </dialog>
-
-
-
     <Modal
       isOpen={isOpenMatchmakingModal}
       onOpenChange={onOpenChangeMatchmakingModal}
@@ -150,9 +76,6 @@ const WaitingModal = ({
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1">
-
-            </ModalHeader>
             <ModalBody>
               <div className="text-center flex flex-col items-center justify-center">
                 {modalStatus != 'success' && (
