104a9fb9e68f0ef853cf0b9d8824183ec9fc2853
Lu Yiting <e0774189@u.nus.edu>
2023-10-24 07:49:46 +0800

    Update history schema

--- history-service/models/History.js
+++ history-service/models/History.js
@@ -4,9 +4,13 @@ const Schema = mongoose.Schema;
 
 const HistorySchema = new Schema({
   userId: { type: String, required: true },
+  sessionId: { type: String, required: true },
   questionId: { type: Number, required: true },
-  win: { type: Boolean, required: true },
+  win: { type: Boolean, required: true },//012
+  score: { type: Number, required: true },
   attemptDate: { type: Date, default: Date.now, required: true },
+  code: { type: String, required: true },
+  feedback: { type: String, reuqired: true}
 });
 
 module.exports = mongoose.model('History', HistorySchema);
