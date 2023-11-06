5a8489de055e5d03d9884293137ea5bb5fe8a64c
imraj singh <92265647+imrajsinghsandhu@users.noreply.github.com>
2023-11-07 01:12:59 +0800

    whitelist certain IPs to access services via CORs

--- chat-service/index.js
+++ chat-service/index.js
@@ -4,7 +4,39 @@ const cors = require("cors");
 const app = express();
 const http = require('http');
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:8000',
+  'http://localhost:8001',
+  'http://localhost:8002',
+  'http://localhost:8006',
+  // node ip
+  'http://34.123.40.181:30800',
+  'http://34.123.40.181:30700',
+  'http://34.123.40.181:30600',
+  'http://34.123.40.181:30500',
+  'http://34.123.40.181:30400',
+  'http://34.123.40.181:30300',
+  'http://34.123.40.181:30200',
+  'http://34.123.40.181:30100',
+  'http://34.123.40.181:30000',
+  // frontend ip
+  'http://34.68.28.7:3000',
+];
+
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const firebaseConfigRoute = require('./routes/firebaseConfig-route');
