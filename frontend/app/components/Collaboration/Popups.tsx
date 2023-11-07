c07f3a949cad5b9adfb5ff63e5a888a197cb50d5
wengkit1 <soowengkit.00@gmail.com>
2023-11-08 04:09:50 +0800

    Bug fix
    Fixed default modal close behaviour.

--- frontend/app/components/Collaboration/Popups.tsx
+++ frontend/app/components/Collaboration/Popups.tsx
@@ -132,7 +132,7 @@ export const WaitingPopup: React.FC<WaitingPopupProps> = ({
   }
 
   return (
-    <Modal size={'lg'} isOpen={isOpen} onOpenChange={onOpenChange}>
+    <Modal size={'lg'} isOpen={isOpen} onOpenChange={onOpenChange} onClose ={()=>{onCancel(); onclose}}>
       <ModalContent>
         {(onClose) => (
           <>
